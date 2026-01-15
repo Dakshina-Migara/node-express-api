832dd6c95574b8f9afe546db652ee870f710b188
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-15 08:47:01 +0530

    api request come to the index..then it goes to the routes

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const employeeRoutes = require('./routes/employeeRoutes');
+
+app.use('/api/v1/employee', employeeRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello migara!')
 })
